2ff3f01835fdd40b16798996a42ab8a5f7b1ddd5
szep <szepiet33@gmail.com>
2025-11-08 11:41:11 +0100

    add public_key field to database

--- sql-database/init.sql
+++ sql-database/init.sql
@@ -4,6 +4,7 @@ CREATE TABLE Users (
     username VARCHAR(50) UNIQUE NOT NULL,
     password_hash TEXT NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    public_key TEXT NOT NULL
 );
 
 -- Create the Texts table with a foreign key to the Users table
