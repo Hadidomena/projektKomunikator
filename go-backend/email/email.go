e00eb4aff5b9fa578631ebc48b555d893f053f7f
szep <szepiet33@gmail.com>
2025-12-05 20:38:28 +0100

    continue work on email

--- go-backend/email/email.go
+++ go-backend/email/email.go
@@ -1,9 +1,32 @@
 package email
 
+import (
+	"crypto/tls"
+	"fmt"
+	"net/smtp"
+
+	"github.com/jordan-wright/email"
+)
+
 func VerifyEmail(email string) bool {
 	return true
 }
 
-func SendEmail(recipient, template string) {
+func SendEmail(recipient, template string) error {
+	e := email.NewEmail()
+	e.From = "Sender Name <sender@example.com>"
+	e.To = []string{"recipient@example.com"}
+	e.Subject = "Test"
+	e.Text = []byte("Hello from Go")
 
+	auth := smtp.PlainAuth("", "smtp-user", "smtp-pass", "smtp.example.com")
+	err := e.SendWithTLS("smtp.example.com:587", auth, &tls.Config{InsecureSkipVerify: false, ServerName: "smtp.example.com"})
+	if err != nil {
+		return fmt.Errorf("failed to create cipher block: %w", err)
+	}
+	return nil
+}
+
+func generateVerificationCode() string {
+	return "einzweipolizei,dreiviergrenadier"
 }
--- go-backend/go.mod
+++ go-backend/go.mod
@@ -7,4 +7,7 @@ require (
 	golang.org/x/crypto v0.43.0
 )
 
-require golang.org/x/sys v0.37.0 // indirect
+require (
+	github.com/jordan-wright/email v4.0.1-0.20210109023952-943e75fe5223+incompatible
+	golang.org/x/sys v0.37.0 // indirect
+)
